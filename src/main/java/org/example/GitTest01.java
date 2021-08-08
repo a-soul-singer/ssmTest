cddc1c0041550050f641e7067fe0165db59be4f3
rick <rick@163.com>
2021-08-08 16:48:37 +0800

    push commit

--- src/main/java/org/example/GitTest01.java
+++ src/main/java/org/example/GitTest01.java
@@ -8,5 +8,6 @@ public class GitTest01 {
         System.out.println("Hello World!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
