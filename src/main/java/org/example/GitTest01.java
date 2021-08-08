f90aa47291e2297c58920059374ba93cc0aac006
rick <rick@163.com>
2021-08-08 16:23:22 +0800

    hot-fix commit1

--- src/main/java/org/example/GitTest01.java
+++ src/main/java/org/example/GitTest01.java
@@ -5,5 +5,6 @@ public class GitTest01 {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
