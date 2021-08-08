e8e73e8595c0ec63819aff8b19cd0749a84aa4f2
rick <rick@163.com>
2021-08-08 16:27:27 +0800

    master commit2

--- src/main/java/org/example/GitTest01.java
+++ src/main/java/org/example/GitTest01.java
@@ -6,5 +6,6 @@ public class GitTest01 {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("master test");
     }
 }
